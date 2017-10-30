d8c21bc6381d00fed057eb9eaa50eccd70b02c50
Yohanes Satrio Dewangga <anggalol27@gmail.com>
2017-10-30 12:27:10 +0700

    sigup sigin

--- app/src/main/java/android/project/techno/otovent_android/signin.java
+++ app/src/main/java/android/project/techno/otovent_android/signin.java
@@ -1,8 +1,11 @@
 package android.project.techno.otovent_android;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.project.techno.otovent_android.API.Impl.ServiceImpl;
+import android.project.techno.otovent_android.API.Service;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -12,27 +15,36 @@ import android.widget.TextView;
  */
 
 public class signin extends AppCompatActivity {
-
+    private ProgressDialog progressDialog = null;
     TextView create;
+    TextView login;
     Typeface fonts1;
-
+    Service service;
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin);
-
-        create = (TextView)findViewById(R.id.create);
-
-        create.setOnClickListener(new View.OnClickListener(){
+        progressDialog = new ProgressDialog(signin.this,R.style.AppTheme);
+        service = new ServiceImpl();
+        create = (TextView) findViewById(R.id.create);
+        login = (TextView) findViewById(R.id.signin1);
+        create.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent it = new Intent(signin.this, signup.class);
                 startActivity(it);
             }
         });
-
-        fonts1 = Typeface.createFromAsset(signin.this.getAssets(), "fonts/Lato-Regular.ttf");
-        TextView t4 = (TextView)findViewById(R.id.create);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                service.authToBackend("users/login","aldi","wkadokaw",signin.this,progressDialog);
+            }
+        });
+        fonts1 = Typeface.createFromAsset(signin.this.getAssets(),
+                "fonts/Lato-Regular.ttf");
+        TextView t4 = (TextView) findViewById(R.id.create);
         t4.setTypeface(fonts1);
     }
+
 }
--- app/src/main/java/android/project/techno/otovent_android/signup.java
+++ app/src/main/java/android/project/techno/otovent_android/signup.java
@@ -1,27 +1,43 @@
 package android.project.techno.otovent_android;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.project.techno.otovent_android.API.Impl.ServiceImpl;
+import android.project.techno.otovent_android.API.Service;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Miura on 10/29/2017.
  */
 
 public class signup extends AppCompatActivity {
-
     TextView signinhere;
+    TextView signUp;
     Typeface fonts1;
-
+    EditText email;
+    EditText username;
+    EditText password;
+    Service service;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
-
-        signinhere = (TextView)findViewById(R.id.signinhere);
+        signinhere = (TextView) findViewById(R.id.signinhere);
+        signUp = (TextView) findViewById(R.id.signup1);
+        email = (EditText) findViewById(R.id.email);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        progressDialog = new ProgressDialog(signup.this,R.style.AppTheme);
+        service = new ServiceImpl();
 
         signinhere.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,9 +47,21 @@ public class signup extends AppCompatActivity {
             }
         });
 
-        fonts1 =  Typeface.createFromAsset(signup.this.getAssets(),"fonts/Lato-Regular.ttf");
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String,String> postBody = new HashMap<String, String>();
+                postBody.put("email",email.getText().toString());
+                postBody.put("username",username.getText().toString());
+                postBody.put("password",password.getText().toString());
+                service.addOrEditUser("users/add",
+                        postBody,signup.this,progressDialog);
+            }
+        });
 
-        TextView t1 =(TextView)findViewById(R.id.signinhere);
+        fonts1 = Typeface.createFromAsset(signup.this.getAssets(),
+                "fonts/Lato-Regular.ttf");
+        TextView t1 = (TextView) findViewById(R.id.signinhere);
         t1.setTypeface(fonts1);
     }
 }
