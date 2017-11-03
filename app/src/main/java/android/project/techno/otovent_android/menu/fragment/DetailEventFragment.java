e9b29353d75a5c583227d46ea389db5dae833835
andrewnapitupulu <andrew.eyeshield@gmail.com>
2017-11-03 10:50:29 +0700

    add fragment detail event and notification

---
+++ app/src/main/java/android/project/techno/otovent_android/menu/fragment/DetailEventFragment.java
@@ -0,0 +1,22 @@
+package android.project.techno.otovent_android.menu.fragment;
+
+import android.os.Bundle;
+import android.project.techno.otovent_android.R;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by N-REW on 03/11/2017.
+ */
+public class DetailEventFragment {
+
+    public DetailEventFragment(){
+
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_detail_event,container,false);
+    }
+}
