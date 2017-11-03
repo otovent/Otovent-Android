6cba26fd7dcea04a6af3e0e317979cccbc3c20f0
andrewnapitupulu <andrew.eyeshield@gmail.com>
2017-11-03 08:17:08 +0700

    add edit profile fragment class, add notification layout

--- app/src/main/java/android/project/techno/otovent_android/menu/fragment/EditProfileFragment.java
+++ app/src/main/java/android/project/techno/otovent_android/menu/fragment/EditProfileFragment.java
@@ -1,7 +1,22 @@
 package android.project.techno.otovent_android.menu.fragment;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.project.techno.otovent_android.R;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by N-REW on 02/11/2017.
  */
-public class EditProfileFragment {
+public class EditProfileFragment extends Fragment {
+
+    public EditProfileFragment(){
+
+    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+    }
 }
