ed4ab72e98aef11fab7eac6ba01fa680b5df16f6
igprad <ign.aldi.pradana@gmail.com>
2017-11-10 15:58:52 +0700

    Added Shared preference for user already logged in in main activity and service impl for auth backend method

--- app/src/main/java/android/project/techno/otovent_android/API/Impl/ServiceImpl.java
+++ app/src/main/java/android/project/techno/otovent_android/API/Impl/ServiceImpl.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.project.techno.otovent_android.API.Service;
 import android.project.techno.otovent_android.API.UserRequest;
 import android.project.techno.otovent_android.R;
@@ -55,6 +56,20 @@ public class ServiceImpl implements Service{
                     String result = response.getString("message");
                     Toast.makeText(callingClass, result, Toast.LENGTH_SHORT).show();
                     if (result.equalsIgnoreCase("Success Login")){
+                        UserRequest userAuthed = new UserRequest();
+                        JSONObject user = response.getJSONArray("result").getJSONObject(0);
+                        userAuthed.setId(user.getLong("id"));
+                        userAuthed.setEmail(user.getString("email"));
+                        userAuthed.setFirstName(user.getString("firstName"));
+                        userAuthed.setLastName(user.getString("lastName"));
+                        userAuthed.setPassword(user.getString("password"));
+
+                        //Save credential to shared preference
+                        SharedPreferences sp = callingClass.getSharedPreferences("user",Context.MODE_PRIVATE);
+                        SharedPreferences.Editor ed = sp.edit();
+                        ed.putLong("ID",userAuthed.getId());
+                        ed.commit();
+
                         Intent it = new Intent(callingClass,BaseActivity.class);
                         callingClass.startActivity(it);
                     } else {
